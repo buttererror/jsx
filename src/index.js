f22f073be2bbb3e735412594bd12003f96fbae49
Mahmoud <sswsy2006@gmail.com>
2019-04-08 21:22:05 +0200

    working on

--- src/index.js
+++ src/index.js
@@ -2,7 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-   return <div>Hello world!</div>;
+   return (
+      <div>
+         <label htmlFor="name">Name: </label>
+         <input type="text" id="name"/>
+         <button style={{'background-color': 'blue', 'color': 'white'}}>Submit</button>
+      </div>
+   );
 };
 
 ReactDOM.render(<App />, document.querySelector("#root"));
